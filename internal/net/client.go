738abecd0fac215fedac24cefef02b8291407811
ThinkerWen <296854007@qq.com>
2024-12-18 21:20:33 +0800

    feat: compression

--- internal/net/client.go
+++ internal/net/client.go
@@ -18,10 +18,10 @@ func init() {
 			SetRetryCount(3).
 			SetRetryWaitTime(500 * time.Millisecond).
 			SetRetryMaxWaitTime(2 * time.Second).
-			SetTimeout(30 * time.Second).
+			SetTimeout(300 * time.Second).
 			SetTransport(&http.Transport{
-				MaxIdleConns:          100,
-				MaxIdleConnsPerHost:   10,
+				MaxIdleConns:          500,
+				MaxIdleConnsPerHost:   50,
 				IdleConnTimeout:       90 * time.Second,
 				TLSHandshakeTimeout:   10 * time.Second,
 				ExpectContinueTimeout: 1 * time.Second,
